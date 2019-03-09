6e1d7fb2ffd43c8af0d554216a57473d7b175b26
C58810 <C58810@NLLH4000520574.solon.prd>
2019-03-09 21:00:43 +0100

    Commit 2

--- JAVATEST/src/day1/Employee.java
+++ JAVATEST/src/day1/Employee.java
@@ -7,4 +7,12 @@ void calculateSalary() {
 	int totalsal=sal+bonus;
 	System.out.println(+totalsal);
 }
+	int calculateSalary1() {
+		int totalsal=sal+bonus;
+		return totalsal;
+	}	
+		int calculateSalary2(int sa,int bon) {
+			int totalsal=sa+bon;
+			return totalsal;
+}
 }
--- JAVATEST/src/day1/demoEmployee.java
+++ JAVATEST/src/day1/demoEmployee.java
@@ -8,6 +8,10 @@ public class demoEmployee {
 munish.sal=7000;
 munish.bonus=700;
 munish.calculateSalary();
+int sal=munish.calculateSalary1();
+System.out.println(+sal);
+int sal1=munish.calculateSalary2(6500,500);
+System.out.println(+sal1);
 	}
 
 }
