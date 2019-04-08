98b1fb4c97ecea9dec7a64336ae559274e901411
goformunish <41254403+goformunish@users.noreply.github.com>
2019-04-08 20:21:13 +0200

    commit

--- JVB/src/clonearr.java
+++ JVB/src/clonearr.java
@@ -2,7 +2,7 @@
 public class clonearr {
 
 	public static void main(String[] args) {
-		int arr[]= {1,2,3,4};
+		int arr[]= {1,2,3,4,5};
 		int clonearr[]=arr.clone();
 		System.out.println(arr!=clonearr);
 for (int i=0;i<clonearr.length;i++)
